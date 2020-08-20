d70cbe25d726e4e83b22eb269111def4b8ff0560
허현 <kkiruru@gmail.com>
2020-08-20 16:09:16 +0900

    key input

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("called main")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		for {
+			timer := time.NewTimer(time.Second * 1)
+			<-timer.C
+			fmt.Println(scanner.Text())
+		}
+	}
+	if scanner.Err() != nil {
+		/*handle error*/
+	}
 }
